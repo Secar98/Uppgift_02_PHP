9978527a138553a55a997f1e867e36171d3619a5
Sebastian Carlson <sebcar98@gmail.com>
2021-04-30 20:13:10 +0200

    fixed API validation

--- App.php
+++ App.php
@@ -7,27 +7,39 @@ header("Referrer-Policy: no-referrer");
 $response = file_get_contents('Data.json');
 $data = json_decode($response, true);
 
-$categorys = array("jewelery", "men clothing", "women clothing", "electronics");
+$categorys = array("all", "jewelery", "men clothing", "women clothing", "electronics");
+$errorsArray = [];
 
-if (!isset($_GET['show']) && !isset($_GET['category'])) {
-    $amount = 20;
-    $category = "all";
-    echo $amount . $category;
-} elseif (check_category($_GET['category'] ?? null, $categorys) && validate_show_is_number($_GET['show'] ?? null)) {
-    $amount = $_GET['show'] ?? null;
-    $category = $_GET['category'] ?? null;
-    echo $amount . $category;
-} else {
-    echo "FEEL";
+$amount = isset($_GET['show']) ? $_GET['show'] : 20;
+$category = isset($_GET['category']) ? $_GET['category'] : "all";
+
+
+validate_is_between($amount) === 0 ? array_push($errorsArray, "Show must be a Number and between 1 - 20") : true;
+check_category($category, $categorys) ? true : array_push($errorsArray, "Not valid category!");
+
+if ($errorsArray) {
+    print_r($errorsArray);
+    exit();
 }
 
-function validate_show_is_number($show)
+function validate_is_between($value)
 {
-    $check_if_number = filter_var($show, FILTER_VALIDATE_INT);
-    return $check_if_number;
+    $result = ($value >= 1 && $value <= 20) ? true : 0;
+    return $result;
 }
+
 function check_category($category, $categorys)
 {
-    $check_category = in_array($category, $categorys);
-    return $check_category;
+    $result2 = in_array($category, $categorys);
+    return $result2;
 }
+
+
+
+//validate_is_number($amount) ? array_push($errorsArray, "Must be a number!") : true;
+/*function validate_is_number($value)
+{
+    $result = filter_var($value, FILTER_VALIDATE_INT);
+    //echo $result;
+    return $result;
+}*/
