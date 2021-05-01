63e90f643b6ec8e8d15e96855b3ceb50bd7c806d
Sebastian Carlson <sebcar98@gmail.com>
2021-05-01 23:09:47 +0200

    made last changes

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once "utils.php";
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
@@ -6,38 +7,41 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 header("Referrer-Policy: no-referrer");
 class API
 {
-    private $categorys = array("all", "jewelery", "men clothing", "women clothing", "electronics");
-    private $errorsArray = array();
-    private $products;
+    private static $categorys = array("all", "jewelery", "men clothing", "women clothing", "electronics");
+    private static $errorsArray = array();
+    private static $products = array();
 
-    public function main($amount, $category)
+    public static function main($amount, $category)
     {
-        $this->validate_show_is_number($amount) ? true : array_push($this->errorsArray, "Show must be a Number!");
-        $this->validate_show($amount) ? true : array_push($this->errorsArray, "Show must be between 1 - 20");
-        $this->validate_category($category) ? true : array_push($this->errorsArray, "Not valid category!");
+        if (!Validation::validate_is_number($amount))
+            array_push(self::$errorsArray, "Show must be a Number!");
+        if (!Validation::validate_amount($amount))
+            array_push(self::$errorsArray, "Show must be between 1 - 20");
+        if (!Validation::validate_category($category, self::$categorys))
+            array_push(self::$errorsArray, "Not valid category!");
 
-        if ($this->errorsArray) {
-            $errors = json_encode($this->errorsArray);
+        if (self::$errorsArray) {
+            $errors = json_encode(self::$errorsArray);
             echo $errors;
             exit();
         }
 
-        $this->products = $this->get_products();
-        $this->products = $category === 'all' ? $this->products : $this->filter_category($this->products, $category);
-        $this->products = $this->limit_products($this->products, $amount);
+        self::$products = self::get_products();
+        self::$products = $category === 'all' ? self::$products : self::filter_category(self::$products, $category);
+        self::$products = self::limit_products(self::$products, $amount);
 
-        $data = json_encode($this->products);
+        $data = json_encode(self::$products);
         echo $data;
     }
 
-    private function get_products()
+    private static function get_products()
     {
         $response = file_get_contents('Data.json');
         $data = json_decode($response, true);
         return $data;
     }
 
-    private function filter_category($products, $category)
+    private static function filter_category($products, $category)
     {
         $filterdProducts = array();
         foreach ($products as $value) {
@@ -48,33 +52,14 @@ class API
         return $filterdProducts;
     }
 
-    private function limit_products($products, $amount)
+    private static function limit_products($products, $amount)
     {
         shuffle($products);
         return array_splice($products, -20, $amount);
-    }
-
-    private function validate_show_is_number($value)
-    {
-        $result = is_numeric($value) ? true : false;
-        return $result;
-    }
-
-    private function validate_show($value)
-    {
-        $result = ($value >= 1 && $value <= 20) ? true : false;
-        return $result;
-    }
-
-    private function validate_category($category)
-    {
-        $result = in_array($category, $this->categorys);
-        return $result;
     }
 }
 
 $amount = isset($_GET['show']) ? htmlspecialchars($_GET['show']) : 20;
 $category = isset($_GET['category']) ? htmlspecialchars($_GET['category']) : "all";
 
-$api = new API();
-$api->main($amount, $category);
+API::main($amount, $category);
